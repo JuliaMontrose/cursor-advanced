ada438d914dc106e8aee37525369d49c8985b640
Yuliia Kolba <yuliia.kolba@gmail.com>
2022-12-14 23:17:38 +0200

    modified script.js

--- HW-2-Cycles/script.js
+++ HW-2-Cycles/script.js
@@ -1,12 +1,12 @@
-let N = parseInt(prompt('Введіть ціле початкове значення: '));
+let N = Number(prompt('Введіть ціле початкове значення: '));
 
-while(N == null || !Number.isInteger(N)) {
-	N = parseInt(prompt('Введіть ЦІЛЕ число без букв'));
+while(!Number.isInteger(N) || isNaN(N) || N == "") {
+	N = Number(prompt('Введіть ЦІЛЕ число без букв'));
 }
 
-let M = parseInt(prompt('Введіть ціле кінцеве значення: '));
-while(M == null || !Number.isInteger(M)) {
-	M = parseInt(prompt('Введіть ЦІЛЕ число без букв'));
+let M = Number(prompt('Введіть ціле кінцеве значення: '));
+while(M == !Number.isInteger(N) || isNaN(N) || N == "" || M <= N) {
+	M = Number(prompt('Введіть ЦІЛЕ число без букв, більше за початкове значення'));
 }
 
 let boolean = confirm('Чи потрібно пропускати парні числа?');
